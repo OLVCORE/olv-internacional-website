fb0dc7d5e9cde5c1e5d84affcb05278334dfdcf6
OLVCORE <olvsistemas@olvinternacional.com.br>
2025-05-22 02:29:24 -0300

    🔗 Adicionando página INDEX otimizada com SEO, design premium e performance

--- js/script.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-  const footer = document.querySelector('footer');
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    footer.classList.add('visible');
-  } else {
-    footer.classList.remove('visible');
-  }
-};
